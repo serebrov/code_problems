7ac25cc0603d0e2914d0b3b74cfbe2f2e92cbd84
Boris Serebrov <serebrov@gmail.com>
2022-11-21 11:12:02 -0500

    Add a note for the xor and sum solution.

--- hackerrank_xor_and_sum/solution.py
+++ hackerrank_xor_and_sum/solution.py
@@ -1,5 +1,12 @@
 # https://www.hackerrank.com/challenges/xor-and-sum/problem
 
+# Note: this is not an proper solution, just a test of a
+# straighforward approach, it works with python.
+# A proper solution would avoid the direct calculation as
+# we are working with large numbers here, the task is
+# in the dynamic programming section which hints that
+# solution could be based on dynamic programming.
+
 a_str = input().strip()
 a = int(a_str, base=2)
 b_str = input().strip()
